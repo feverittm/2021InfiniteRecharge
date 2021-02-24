2f7796c82c77a4c6864063d2bdb218454d01d3dd
Floyd Moore <floyd.moore@hp.com>
2021-02-23 20:21:05 -0800

    updates...

--- src/main/java/frc/robot/commands/shooter/ShooterStreamAutoTarget.java
+++ src/main/java/frc/robot/commands/shooter/ShooterStreamAutoTarget.java
@@ -1,13 +1,20 @@
 package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.Hopper;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Limelight;
+import frc.spartanlib.math.MathUtils;
 
 public class ShooterStreamAutoTarget extends CommandBase {
 
   private double mTarget = 0.0;
-  private long mLastUpdate;
+  //private long mLastUpdate;
+
+  public ShooterStreamAutoTarget(){
+    mTarget = 0.8;
+  }
 
   public ShooterStreamAutoTarget(double speed) {
     mTarget = speed;
@@ -15,25 +22,22 @@ public class ShooterStreamAutoTarget extends CommandBase {
 
   @Override
   public void initialize() {
-    mLastUpdate = System.currentTimeMillis();
+    //mLastUpdate = System.currentTimeMillis();
   }
 
   @Override
   public void execute() {
-    //double deltaT = System.currentTimeMillis() - mLastUpdate;
-    //double output = LimeLight.getInstance().getPIDOutput(deltaT);
-    //DriveTrain.getInstance().setMotors(-output, output);
+    // double deltaT = System.currentTimeMillis() - mLastUpdate;
+    // double output = LimeLight.getInstance().getPIDOutput(deltaT);
+    // DriveTrain.getInstance().setMotors(-output, output);
 
-    /*
     Shooter.getInstance().setRPM(mTarget);
-    if (MathUtils.epsilon(Shooter.getInstance().getRPMs(), mTarget, 80)
-      && Hopper.getInstance().mBallCount > 0
-      && Math.abs(LimeLight.getInstance().getDouble(LimeLight.TARGET_X, 0)) < Constants.Values.VISION_ANGLE_TOLERANCE) {
+    if (MathUtils.epsilon(Shooter.getInstance().getRPMs(), mTarget, 80) && Hopper.getInstance().mBallCount > 0
+        && Math.abs(Limelight.getInstance().getTX()) < Constants.Values.VISION_ANGLE_TOLERANCE) {
       Hopper.getInstance().setSpeed(Constants.Values.HOPPER_STREAM_SPEED);
     } else {
-    */
       Hopper.getInstance().setSpeed(0.0);
-    //}
+    }
   }
 
   @Override
